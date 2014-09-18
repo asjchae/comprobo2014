73721da076a7920402bc97e82b2e36730ce99a1c
Ariana Chae <achae@slaptop.(none)>
2014-09-18 13:49:56 -0400

    This sucks

--- src/warmup_project/scripts/wall_follower.py
+++ src/warmup_project/scripts/wall_follower.py
@@ -15,7 +15,7 @@ class WallFollower():
         rospy.init_node('wallfollower', anonymous = True)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.sub = rospy.Subscriber('scan', LaserScan, self.scan_around)
-        self.front_distance = -1.0
+        self.front_distance = 1
 
 
     def scan_around(self, msg):
@@ -24,12 +24,12 @@ class WallFollower():
 
 
         # Check front
-        for i in range(45):
-            if msg.ranges[314+i] > 0:
-                check_front.append(msg.ranges[314+i])
-            if msg.ranges[45-i] > 0:
-                check_front.append(msg.ranges[45-i])
-        if len(check_front) > 45:
+        for i in range(15):
+            if msg.ranges[344+i] > 0:
+                check_front.append(msg.ranges[344+i])
+            if msg.ranges[15-i] > 0:
+                check_front.append(msg.ranges[15-i])
+        if len(check_front) > 15:
             self.front_distance = sum(check_front)/float(len(check_front))
 
 
@@ -38,13 +38,16 @@ class WallFollower():
         
         r = rospy.Rate(10) # 10hz
         while not rospy.is_shutdown():
-        	if self.front_distance > .8:
-        		follow_msg = Twist(Vector3(0.1, 0.0, 0.0), Vector3(0.0, 0.0, 0.0))
-        	else:
+        	print self.front_distance
+        	if self.front_distance < .8 or self.front_distance == .8:
+        		print "hello"
         		follow_msg = Twist(Vector3(0.0, 0.0, 0.0), Vector3(0.0, 0.0, 0.1))
+        	else :
+        		follow_msg = Twist(Vector3(0.1, 0.0, 0.0), Vector3(0.0, 0.0, 0.0))
+        		# self.front_distance = 1
 
-            	self.pub.publish(follow_msg)
-            	r.sleep()
+	        self.pub.publish(follow_msg)
+	        r.sleep()
 
 if __name__ == '__main__':
     try:
